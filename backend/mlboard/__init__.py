6db10cff4a6f613738aed09c8a67d22b48fa71a2
Faza Fahleraz <ffahleraz@gmail.com>
2019-09-25 18:14:08 +0700

    Add mongodb

--- backend/mlboard/__init__.py
+++ backend/mlboard/__init__.py
@@ -8,9 +8,7 @@ from . import session
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
-    )
+    app.config.from_mapping(MONGO_URI="mongodb://localhost:27017/ml-board")
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
--- backend/mlboard/session.py
+++ backend/mlboard/session.py
@@ -1,25 +1,38 @@
-from flask import (
-    Blueprint,
-    flash,
-    g,
-    redirect,
-    render_template,
-    request,
-    session,
-    url_for,
-)
+import json
+import uuid
 
-# from flaskr.db import get_db
+from flask import Blueprint, Response, request
 
-bp = Blueprint("auth", __name__, url_prefix="/session")
+from .db import get_db
+
+bp = Blueprint("sessions", __name__, url_prefix="/sessions")
 
 
-@bp.route("/create", methods=["GET"])
+@bp.route("/<session_id>", methods=["GET"])
+def configure(session_id):
+    db = get_db()
+
+    session = db.sessions.find_one({"id": session_id}, {"_id": False})
+    if session is not None:
+        return Response(json.dumps(session), 200)
+    else:
+        return Response("invalid session id", 404)
+
+
+@bp.route("/create", methods=["POST"])
 def create():
-    return "create session"
+    new_id = uuid.uuid4().hex[:8]
+
+    db = get_db()
+    db.sessions.insert({"id": new_id, "status": 0})
+
+    session = db.sessions.find_one({"id": new_id}, {"_id": False})
+    if session is not None:
+        return Response(json.dumps(session), 200)
+    else:
+        return Response("internal error", 500)
 
 
-@bp.route("/configure", methods=["GET"])
-def configure():
-    return "configure session"
-
+@bp.route("/train", methods=["POST"])
+def train():
+    return "train session"
